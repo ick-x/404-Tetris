e638db22b05c14efc28d74cd93a83c62e1b2391e
ickx <mizoudaaa@gmail.com>
2023-12-14 01:03:07 +0100

    Fix speedModifier reset

--- tetris.js
+++ tetris.js
@@ -270,6 +270,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 clearInterval(intervalId);
                 if (confirm('Game Over! \n Do you want to play again?')) {
                     tetrisGrid = newGrid();
+                    speedModifier = 0;
                 } else {
                     open("/404-Tetris/index.html", "_self");
                 }
