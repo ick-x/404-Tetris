0e4d46f5d564d7759ef952dab4aafaee1876c49f
ickx <mizoudaaa@gmail.com>
2023-12-16 13:13:57 +0100

    Fix piece rotate problem & draw grid squares

--- JS/tetris.js
+++ JS/tetris.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let currentPiece;
     let nextPiece;
-    let intervalId;
 
     let ticCounter = 0;
     let speedModifier = 1;
@@ -28,18 +27,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function drawSquare(x, y, color, padding = 0, context = null, blockSize = BLOCK_SIZE) {
-
         if (context == null) {
             context = document.getElementById('tetrisCanvas').getContext('2d');
         }
 
+        // Dessiner le carré avec une bordure et un ombrage
         context.fillStyle = color;
         context.fillRect(x * blockSize, y * blockSize + padding, blockSize, blockSize);
-        context.strokeStyle = '#fff';
-        context.strokeRect(x * blockSize, y * blockSize + padding, blockSize, blockSize);
+
+        // Dessiner une bordure
+        context.strokeStyle = '#ffffff';
+        context.lineWidth = 2;
+        context.strokeRect(x * blockSize + 1, y * blockSize + padding + 1, blockSize - 2, blockSize - 2);
+
+        // Dessiner un ombrage
+        context.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+        context.lineWidth = 2;
+        context.beginPath();
+        context.moveTo(x * blockSize + blockSize, y * blockSize + padding);
+        context.lineTo(x * blockSize + blockSize, y * blockSize + blockSize + padding);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(x * blockSize, y * blockSize + blockSize + padding);
+        context.lineTo(x * blockSize + blockSize, y * blockSize + blockSize + padding);
+        context.stroke();
+
     }
 
 
+    // Draw the grid and the current tetris piece
     // Draw the grid and the current tetris piece
     function drawGridAndPiece() {
         context.clearRect(0, 0, canvas.width, canvas.height);
@@ -51,7 +67,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (color) {
                     drawSquare(col, row, color);
                 }
+                // Draw very fine lines between the grid cells
+                context.beginPath();
+                context.moveTo(col  * BLOCK_SIZE, 0);
+                context.lineTo(col  * BLOCK_SIZE, canvas.height);
+                context.strokeStyle = 'rgba(220,220,220,0.45)';
+                context.stroke();
             }
+            // Draw very fine lines between the grid rows
+            context.beginPath();
+            context.moveTo(0, row * BLOCK_SIZE);
+            context.lineTo(canvas.width, row * BLOCK_SIZE);
+            context.strokeStyle = 'rgba(220,220,220,0.45)';
+            context.stroke();
         }
 
         // Draw the current falling tetris piece
@@ -59,6 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPiece.draw();
         }
     }
+
 
     function drawNextPiece() {
         const nextPieceCanvas = document.getElementById('nextPieceCanvas');
@@ -126,19 +155,55 @@ document.addEventListener('DOMContentLoaded', () => {
         // Move the Tetris piece to the left
         moveLeft() {
             this.col--;
+            if (collision()) {
+                // If collision, revert the move
+                this.col++;
+            }
         }
 
         // Move the Tetris piece to the right
         moveRight() {
             this.col++;
+            if (collision()) {
+                // If collision, revert the move
+                this.col--;
+            }
         }
 
         // Rotate the Tetris piece
+        // Rotate the Tetris piece
         rotate() {
+            const oldShape = this.shape;
+            const oldRow = this.row;
+            const oldCol = this.col;
+
+            // Calculate the center of the piece
+            const centerRow = Math.floor(this.shape.length / 2);
+            const centerCol = Math.floor(this.shape[0].length / 2);
+
+            // Perform the rotation around the center
             this.shape = this.shape[0].map((_, i) =>
                 this.shape.map((row) => row[i]).reverse()
             );
+
+            // Adjust the piece position to keep the center constant
+            this.row = oldRow + centerRow - Math.floor(this.shape.length / 2);
+            this.col = oldCol + centerCol - Math.floor(this.shape[0].length / 2);
+
+            // If the rotation causes a collision, revert the changes
+            if (collision()) {
+                this.col = oldCol - 1;
+                if (collision()) {
+                    // If collision persists, revert the changes
+                    this.shape = oldShape;
+                    this.row = oldRow;
+                    this.col = oldCol;
+                }
+            }
         }
+
+
+
     }
 
     // Check for collision between the Tetris piece and the grid
@@ -158,16 +223,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Merge the Tetris piece into the grid
+    // Merge the Tetris piece into the grid
     function mergePiece() {
         currentPiece.shape.forEach((row, i) => {
             row.forEach((col, j) => {
                 if (col) {
-                    // Merge the piece into the grid
-                    tetrisGrid[currentPiece.row + i - 1][currentPiece.col + j] = currentPiece.color;
+                    // Merge the piece into the grid if it is within bounds
+                    if (tetrisGrid[currentPiece.row + i - 1] && tetrisGrid[currentPiece.row + i - 1][currentPiece.col + j] !== undefined) {
+                        tetrisGrid[currentPiece.row + i - 1][currentPiece.col + j] = currentPiece.color;
+                    }
                 }
             });
         });
     }
+
 
     // Clear completed rows from the grid
     function clearRows() {
@@ -185,6 +254,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Update the game state
     function update() {
+        checkGameOver();
         // Display the score
         displayScore.innerHTML = "Score : " + score;
 
@@ -204,7 +274,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Increment the tic counter and increase speed over time
         ticCounter++;
-        if (ticCounter === 4000 && speedModifier < 4) {
+        if (ticCounter === 10000 * 4 && speedModifier < 4) {
             ticCounter = 0;
             speedModifier++;
         }
@@ -289,24 +359,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // Listen for keydown events
     document.addEventListener('keydown', handleKeyPress);
 
-    // Initialize a new Tetris piece
-
-
-    // Set up the game loop
-    intervalId = setInterval(() => {
-        update();
-        drawGridAndPiece();
-    }, 500); // Adjust the speed as needed
-
     // Check for game over condition
     function checkGameOver() {
         for (let i = 0; i < tetrisGrid[0].length; ++i) {
             if (tetrisGrid[0][i] !== 0) {
-                // Game over: stop the game and ask for a restart
-                clearInterval(intervalId);
                 if (confirm('Game Over! \n Do you want to play again?')) {
                     tetrisGrid = newGrid();
                     speedModifier = 1;
+                    score = 0;
                 } else {
                     open("../index.html", "_self");
                 }
@@ -323,8 +383,8 @@ document.addEventListener('DOMContentLoaded', () => {
         drawGridAndPiece();  // Update UI
         refreshScore(lineCount);
         requestAnimationFrame(gameLoop);  // Repeat the loop
-
     }
+
     // Initialize a new Tetris piece
     currentPiece = new TetrisPiece(getRandomShape(), getRandomColor());
     drawNextPiece();
