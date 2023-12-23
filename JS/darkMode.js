665aee02d14fe513673a6a5a4fd3752ef342594b
ickx <mizoudaaa@gmail.com>
2023-12-23 20:41:44 +0100

    Fix IU

---
+++ JS/darkMode.js
@@ -0,0 +1,36 @@
+const backgroundToggle = document.getElementById('backgroundToggle');
+const gameContainer = document.getElementById('gameContainer');
+const rightKeyImage = document.getElementById('rightKeyImage');
+const leftKeyImage = document.getElementById('leftKeyImage');
+const downKeyImage = document.getElementById('downKeyImage');
+const upKeyImage = document.getElementById('upKeyImage');
+const rKeyImage = document.getElementById('rKeyImage');
+const spaceKeyImage = document.getElementById('spaceKeyImage');
+
+const DKeyImage = document.getElementById('DKeyImage');
+const QKeyImage = document.getElementById('QKeyImage');
+const ZKeyImage = document.getElementById('ZKeyImage');
+const SKeyImage = document.getElementById('SKeyImage');
+
+backgroundToggle.addEventListener('change', function () {
+    const backgroundColor = this.checked ? 'black' : 'white';
+    const textColor = this.checked ? 'white' : 'black';
+    const borderColor = this.checked ? '1px solid white' : '1px solid black';
+    const keyImagePrefix = this.checked ? 'White' : 'Black';
+
+    document.body.style.backgroundColor = backgroundColor;
+    gameContainer.style.color = textColor;
+    document.getElementById("tetrisCanvas").style.border = borderColor;
+
+    // Met à jour les images des commandes
+    rightKeyImage.src = `../images/${keyImagePrefix}/RightKey.png`;
+    leftKeyImage.src = `../images/${keyImagePrefix}/LeftKey.png`;
+    downKeyImage.src = `../images/${keyImagePrefix}/DownKey.png`;
+    upKeyImage.src = `../images/${keyImagePrefix}/UpKey.png`;
+    rKeyImage.src = `../images/${keyImagePrefix}/RKey.png`;
+    spaceKeyImage.src = `../images/${keyImagePrefix}/SpaceKey.png`;
+    QKeyImage.src = `../images/${keyImagePrefix}/QKey.png`;
+    DKeyImage.src = `../images/${keyImagePrefix}/DKey.png`;
+    ZKeyImage.src = `../images/${keyImagePrefix}/ZKey.png`;
+    SKeyImage.src = `../images/${keyImagePrefix}/SKey.png`;
+});
