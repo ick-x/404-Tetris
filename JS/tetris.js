5989f06652056549531a3174ad6b8c30d9ac9d14
LaCho7 <59169832+LaCho7@users.noreply.github.com>
2024-01-23 16:07:03 +0100

    Add comment to tetris js

--- JS/tetris.js
+++ JS/tetris.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Get HTML canvas element
     const canvas = document.getElementById('tetrisCanvas');
+    // 2D Rendering context
     const context = canvas.getContext('2d');
 
+    // Grid and block size constants
     const ROWS = 20;
     const COLUMNS = 10;
     const BLOCK_SIZE = 30;
 
+    // Define tetromino shapes
     const shapes = {
         square: [[1, 1], [1, 1]],
         line: [[1, 1, 1, 1]],
@@ -16,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         Z: [[0, 1, 1], [1, 1, 0]],
     };
 
+    // Definition of tetromino color
     const shapeColors = {
         square: '#eaea00',
         line: '#00efef',
@@ -26,26 +31,40 @@ document.addEventListener('DOMContentLoaded', () => {
         Z: '#FF0000',
     };
 
+    // Initialize the Tetris grid
     let tetrisGrid = newGrid();
 
-    let pieceIntermediaire;
+    // Variables to manage current, next, and saved tetris pieces
+    let intermediatePiece;
     let currentPiece;
     let nextPiece;
     let savedPiece;
 
+    // Variables to keep track of game state
     let nbPieces = 1;
     let speedModifier = 1;
-
     let lineCount = 0;
     let score = 0;
     let displayScore = document.getElementById("displayScore");
+    let timePerFrame = 4;
 
-
+    /**
+     * 
+     * @returns Array - 2D array representing the Tetris grid
+     */
     function newGrid() {
-        return Array.from({length: ROWS}, () => Array(COLUMNS).fill(0));
+        return Array.from({ length: ROWS }, () => Array(COLUMNS).fill(0));
     }
 
-
+    /**
+     * Draws a colored square with optional padding
+     * @param {*} x coordinate of the top-left corner of the square
+     * @param {*} y coordinate of the top-left corner of the square
+     * @param {*} color color of the sqaure
+     * @param {*} padding optional padding around the square
+     * @param {*} context optional render context
+     * @param {*} blockSize optional size of each block
+     */
     function drawSquare(x, y, color, padding = 0, context = null, blockSize = BLOCK_SIZE) {
         if (context == null) {
             context = document.getElementById('tetrisCanvas').getContext('2d');
@@ -54,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
         context.fillStyle = color;
         context.fillRect(x * blockSize, y * blockSize + padding, blockSize, blockSize);
 
-            context.strokeStyle = '#ffffff';
+        context.strokeStyle = '#ffffff';
         context.lineWidth = 2;
         context.strokeRect(x * blockSize + 1, y * blockSize + padding + 1, blockSize - 2, blockSize - 2);
 
@@ -71,23 +90,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    /**
+     * Draws the tetris grid and the current Tetromino
+     */
     function drawGridAndPiece() {
+        // Clear the entire canvas
         context.clearRect(0, 0, canvas.width, canvas.height);
 
+        // Draw the Tetris grid
         for (let row = 0; row < ROWS; row++) {
             for (let col = 0; col < COLUMNS; col++) {
                 const color = tetrisGrid[row][col];
                 if (color) {
                     drawSquare(col, row, color);
                 }
-
+                // Draw vertical grid lines
                 context.beginPath();
                 context.moveTo(col * BLOCK_SIZE, 0);
                 context.lineTo(col * BLOCK_SIZE, canvas.height);
                 context.strokeStyle = 'rgba(220,220,220,0.45)';
                 context.stroke();
             }
-
+            // Draw horizontal grid lines
             context.beginPath();
             context.moveTo(0, row * BLOCK_SIZE);
             context.lineTo(canvas.width, row * BLOCK_SIZE);
@@ -95,24 +119,31 @@ document.addEventListener('DOMContentLoaded', () => {
             context.stroke();
         }
 
+        // Draw the current Tetromino
         if (currentPiece) {
             currentPiece.draw();
         }
     }
 
-
+    /**
+     * Draws the next Tetromino on a separate canvas
+     */
     function drawNextPiece() {
         const nextPieceCanvas = document.getElementById('nextPieceCanvas');
         const nextPieceContext = nextPieceCanvas.getContext('2d');
 
+        // Clear the "nextPieceCanvas"
         nextPieceContext.clearRect(0, 0, nextPieceCanvas.width, nextPieceCanvas.height);
 
+        // Get a random Tetromino for the next piece and his color
         const nextShape = getRandomShape();
         const nextColor = getColor(nextShape);
 
+        // Instance the new Tetromino with his class
         nextPiece = new TetrisPiece(nextShape, nextColor);
         nbPieces++;
 
+        // Draw the new Tetromino on the "nextPieceCanvas"
         nextPiece.shape.forEach((row, i) => {
             row.forEach((col, j) => {
                 if (col) {
@@ -121,13 +152,16 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-
+    /**
+     * Draws the saved Tetromino on a separate canvas
+     */
     function drawSavedPiece() {
         const savedPieceCanvas = document.getElementById('savedPieceCanvas');
         const savedPieceContext = savedPieceCanvas.getContext('2d');
 
         const savedColor = savedPiece.color;
 
+        // Draw the saved piece on the "savedPieceCanvas"
         savedPiece.shape.forEach((row, i) => {
             row.forEach((col, j) => {
                 if (col) {
@@ -137,17 +171,33 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    /**
+     * Clears the saved Tetromino
+     */
     function clearSavedPiece() {
         const savedPieceCanvas = document.getElementById('savedPieceCanvas');
         const savedPieceContext = savedPieceCanvas.getContext('2d');
         savedPieceContext.clearRect(0, 0, savedPieceCanvas.width, savedPieceCanvas.height);
     }
 
+    /**
+     * Get the color associated with a Tetromino
+     * @param {string} shape - Tetromino shape 
+     * @returns {string} - Color code
+     */
     function getColor(shape) {
         return shapeColors[shape];
     }
 
+    /**
+     * Class Representing a Tetromino
+     */
     class TetrisPiece {
+        /**
+         * Constructor of a Tetromino
+         * @param {string} shape - Tetromino shape 
+         * @param {string} color - Color code
+         */
         constructor(shape, color) {
             this.frame = 0;
             this.maxFrame = 240;
@@ -157,6 +207,9 @@ document.addEventListener('DOMContentLoaded', () => {
             this.col = Math.floor(COLUMNS / 2) - Math.floor(this.shape[0].length / 2);
         }
 
+        /**
+         * Draws the Tetromino on the principal canvas
+         */
         draw() {
             this.shape.forEach((row, i) => {
                 row.forEach((col, j) => {
@@ -167,6 +220,9 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
 
+        /**
+         * Moves the Tetromino down automatically
+         */
         autoMoveDown() {
             clearRows();
             this.frame += speedModifier;
@@ -176,11 +232,17 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
+        /**
+         * Moves the Tetromino down manually 
+         */
         moveDown() {
             this.row++;
             this.frame = 0;
         }
 
+        /**
+         * Moves the Tetromino to the left manually
+         */
         moveLeft() {
             this.col--;
             if (collision()) {
@@ -188,12 +250,19 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
+        /**
+         * Moves the Tetromino to the right manually
+         */
         moveRight() {
             this.col++;
             if (collision()) {
                 this.col--;
             }
         }
+
+        /**
+         * Rotate the Tetromino manually
+         */
         rotate() {
             const oldShape = this.shape;
             const oldRow = this.row;
@@ -202,35 +271,47 @@ document.addEventListener('DOMContentLoaded', () => {
             const centerRow = Math.floor(this.shape.length / 2);
             const centerCol = Math.floor(this.shape[0].length / 2);
 
+            // Rotate the shape
             this.shape = this.shape[0].map((_, i) =>
                 this.shape.map((row) => row[i]).reverse()
             );
             this.row = oldRow + centerRow - Math.floor(this.shape.length / 2);
             this.col = oldCol + centerCol - Math.floor(this.shape[0].length / 2);
+
+            // Check for collision after rotation
             if (collision()) {
                 recenterToBoard(oldCol);
                 if (collision()) {
+                    // Restore the old shape and position if collision
                     this.shape = oldShape;
                     this.row = oldRow;
                     this.col = oldCol;
                 }
             }
         }
-
-
     }
-    function recenterToBoard(oldCol){
-        const liste = [-1,-2,0,1,2]
+
+    /**
+     * Recenter the tetromino to the board after rotation
+     * @param {number} oldCol - Original column position
+     * @returns 
+     */
+    function recenterToBoard(oldCol) {
+        const liste = [-1, -2, 0, 1, 2]
         const initialCol = currentPiece.col;
-        for(let offset of liste){
+        for (let offset of liste) {
             currentPiece.col += offset;
-            if(!collision()){
+            if (!collision()) {
                 return;
             }
-            currentPiece.col=initialCol;
+            currentPiece.col = initialCol;
         }
     }
 
+    /**
+     * Check for collision between a Tetromino and the filled blocks in the grid
+     * @returns {boolean} - True if collision is detected, false otherwise
+     */
     function collision() {
         for (let i = 0; i < currentPiece.shape.length; i++) {
             for (let j = 0; j < currentPiece.shape[i].length; j++) {
@@ -246,6 +327,9 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     }
 
+    /**
+     * Merges the current tetromino into the Tetris grid
+     */
     function mergePiece() {
         currentPiece.shape.forEach((row, i) => {
             row.forEach((col, j) => {
@@ -258,6 +342,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    /**
+     * Clears completed rows from the Tetris grid
+     */
     function clearRows() {
         for (let row = ROWS - 1; row >= 0; row--) {
             if (tetrisGrid[row].every((col) => col !== 0)) {
@@ -268,6 +355,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Update the game state and checks for game
+     */
     function update() {
         checkGameOver();
         displayScore.innerHTML = "Score : " + score;
@@ -281,21 +371,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 drawNextPiece();
             }
         }
-
+        // Added speed every 25 Tetromino 
         if (nbPieces === 25) {
             nbPieces = 0;
             speedModifier++;
         }
-
     }
 
+    /**
+     * Gets a random Tetris shape
+     * @returns {string} - Random Tetris shape
+     */
     function getRandomShape() {
         const shapes = Object.keys(shapeColors);
         const randomIndex = Math.floor(Math.random() * shapes.length);
         return shapes[randomIndex];
     }
 
-
+    /**
+     * Updates the score based on the number of cleared lines
+     * @param {number} lineCounter - number of lines cleared
+     */
     function refreshScore(lineCounter) {
         switch (lineCounter) {
             case 1:
@@ -317,9 +413,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Handless key presses to control the Tetromino
+     * @param {KeyboardEvent} event - Key press event 
+     */
     function handleKeyPress(event) {
         switch (event.code) {
-            case 'ArrowLeft' :
+            case 'ArrowLeft':
                 currentPiece.moveLeft();
                 if (collision()) currentPiece.moveRight();
                 break;
@@ -339,7 +439,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 currentPiece.rotate();
                 if (collision()) currentPiece.rotate();
                 break;
-            case 'KeyA' :
+            case 'KeyA':
                 currentPiece.moveLeft();
                 if (collision()) currentPiece.moveRight();
                 break;
@@ -370,9 +470,9 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'KeyR':
                 if (savedPiece) {
                     clearSavedPiece();
-                    pieceIntermediaire = currentPiece;
+                    intermediatePiece = currentPiece;
                     currentPiece = savedPiece;
-                    savedPiece = pieceIntermediaire;
+                    savedPiece = intermediatePiece;
                     drawSavedPiece();
                     currentPiece.row = -currentPiece.shape.length + 1;
                     currentPiece.col = Math.floor(COLUMNS / 2) - Math.floor(currentPiece.shape[0].length / 2);
@@ -388,37 +488,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Listen for keydown events to control Tetromino
     document.addEventListener('keydown', handleKeyPress);
 
+    /**
+     * Checks if the game is over by inspecting the top row of the tetris grid
+     * If the top row contains any filles blocks, prompts the user to play again
+     * Resets the game state if the user chooses to play again
+     * Otherwise redirect user to the homepage
+     */
     function checkGameOver() {
+        // Iterate through the top row of the Tetris grid
         for (let i = 0; i < tetrisGrid[0].length; ++i) {
+            // If a filled block is found in the top row, the game is over
             if (tetrisGrid[0][i] !== 0) {
+                // Prompt for user with a confirmation dialog
                 if (confirm('Game Over! \n Do you want to play again?')) {
+                    // Reset the game
                     tetrisGrid = newGrid();
                     speedModifier = 1;
                     score = 0;
                     clearSavedPiece();
                 } else {
+                    //Redirect to the homepage
                     open("../index.html", "_self");
                 }
             }
         }
     }
 
-
+    /**
+     * Continuouslt updates and renders the game
+     */
     function gameLoop() {
+        // Check for GameOver conditions
         checkGameOver();
-        for (let i = 0; i < 4; i++) {
+
+        // Update game state four times per frame
+        for (let i = 0; i < timePerFrame; i++) {
             update();
         }
+
+        // Draw the Tetris grid and current piece
         drawGridAndPiece();
+        // Refresh the score
         refreshScore(lineCount);
         requestAnimationFrame(gameLoop);
 
     }
 
+    // Initialize the current tetromino and draw the next piece
     currentPiece = new TetrisPiece(getRandomShape(), getColor());
     drawNextPiece();
 
+    // Start the gameLoop
     gameLoop();
 });
